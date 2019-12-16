4e79900bbc46c239859eabe05e5743c8a7855d82
Summabonas <51034065+Summabonas@users.noreply.github.com>
2019-12-16 14:21:26 +0100

    Update L1 and L2 Regularization

---
+++ Optimization/Constraints.py
@@ -0,0 +1,30 @@
+import numpy as np
+class L1_Regularizer:
+    def __init__(self, alpha):
+        self.alpha = alpha          #regularization parameter
+
+    def calculate_gradient(self, weights):
+        grad = self.alpha * np.sign(weights)
+        return grad
+
+    def norm(self, weights):
+        a = np.sum(np.absolute(weights))
+        loss = self.alpha *  a        #||a||_1 = sum(|a_i|)
+        return loss
+
+class L2_Regularizer:
+    def __init__(self, alpha):
+        self.alpha = alpha
+
+    def calculate_gradient(self, weights):
+        #grad = 2 * self.alpha * weights        # mathematical form
+        grad = self.alpha * weights             # engineering form
+        return grad
+
+    def norm(self, weights):
+        a=np.sum(np.square(weights))
+        #loss = self.alpha * np.sum(np.square(weights))      #||a||_2 = sqrt(sum(a_i^2))
+        loss = self.alpha * np.sqrt(np.sum(np.square(weights)))    #????????
+        return loss
+
+
