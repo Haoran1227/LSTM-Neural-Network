fc8b2261dd776d85e53bf5d3806a1977568df3d0
Summabonas <51034065+Summabonas@users.noreply.github.com>
2019-12-18 11:37:55 +0100

    Update NeuralNetwork

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -10,12 +10,17 @@ class NeuralNetwork:
         self.label_tensor = None            #Data load function must be operated one time in one training iteration
         self.weights_initializer = weights_initializer
         self.bias_initializer = bias_initializer
+        self._phase = None
 
     def forward(self):
         input_tensor, self.label_tensor = self.data_layer.forward()         # see IrisData class in Helpers.py
+        regularization_loss = 0         # record regularization loss
         for layer in self.layers:
             input_tensor = layer.forward(input_tensor)                      #forward propagation
-        loss = self.loss_layer.forward(input_tensor, self.label_tensor)     # use the output of network to do optimization
+            if layer.weights is not None:      # it's a trainable layer
+                if layer.optimizer.regularizer is not None:     # it has regularizer
+                    regularization_loss += layer.optimizer.regularizer.norm(layer.weights)
+        loss = self.loss_layer.forward(input_tensor, self.label_tensor) + regularization_loss    # use the output of network to do optimization
         return loss                                                         #the consequence is loss of network
 
     def backward(self,label_tensor):
@@ -36,14 +41,26 @@ class NeuralNetwork:
         self.layers.append(layer)
 
     def train(self,iterations):
+        self.phase = "train"
         for i in range(iterations):
             self.loss.append(self.forward())
             self.backward(self.label_tensor)
 
     def test(self,input_tensor):
+        self.phase = "test"
         for layer in self.layers:
             input_tensor = layer.forward(input_tensor)              # forward propagation
         return input_tensor
+
+# phase property to set "train" or "test" for each layer
+    @property
+    def phase(self):
+        return self._phase
+
+    @phase.setter
+    def phase(self, condition):
+        for layer in self.layers:
+            layer.phase = condition
 
 
 
