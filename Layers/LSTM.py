52ec33fe2fc46522e83c315d553e478d2575036a
Summabonas <51034065+Summabonas@users.noreply.github.com>
2019-12-27 23:17:11 +0800

    Update LSTM comments

--- Layers/LSTM.py
+++ Layers/LSTM.py
@@ -5,6 +5,19 @@ from Layers.TanH import TanH
 from Layers.Sigmoid import Sigmoid
 
 class LSTM_cell:
+    """
+    This class defines one LSTM-cell which is used to build a LSTM layer.
+    It includes the realization of forward and backward propagation algorithm
+    Attributes:
+        W_xh, W_hh, B_h, W_hy, B_y: trainable parameters of LSTM-cell. Because LSTM layer shares
+            weights in sequential LSTM cells, we need to make all LSTM cells in the same layer have
+            the same weights and the update of weights needs to be implemented after calculating all
+            gradients of cells.
+        sigmoid: store sigmoid activation functions so that the variables can be transferred from
+            forward pass to backward pass
+        tanh: store tanh activation functions
+        cache: store the variables in forward pass for the gradient calculation in backward pass.
+    """
     def __init__(self, W_xh, W_hh, B_h, W_hy, B_y):
         # W_xh = [W_xf W_xi W_xc W_xo].T    shape:(4H, J)   each W:(H, J)
         # W_hh = [W_hf W_hi W_hc W_ho].T    shape:(4H, H)
@@ -105,24 +118,23 @@ class LSTM_cell:
 
 
 class LSTM(base_layer):
+    """
+    This class build LSTM layer based on LSTM-cell class.
+    """
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.J, self.H, self.K = input_size, hidden_size, output_size
 
-        # W_xh = [W_xf W_xi W_xc W_xo].T    shape:(4H, J)   W-(H, J)
-        # W_hh = [W_hf W_hi W_hc W_ho].T    shape:(4H, H)
-        # B_h = [B_xf B_xi B_xc B_xo]       shape:(1, 4H)   B-(1, H)
-        # W_hy  shape:(K, H)
-        # B_y   shape:(1, K)
-        self.W_xh = np.random.rand(4*self.H, self.J)
-        self.W_hh = np.random.rand(4*self.H, self.H)
-        self.B_h = np.random.rand(1, 4*self.H)
-        self.W_hy = np.random.rand(self.K, self.H)
-        self.B_y = np.random.rand(1, self.K)
+        # initialize trainable parameters
+        self.W_xh = np.random.rand(4*self.H, self.J)    # W_xh = [W_xf W_xi W_xc W_xo].T    shape:(4H, J)   W-(H, J)
+        self.W_hh = np.random.rand(4*self.H, self.H)    # W_hh = [W_hf W_hi W_hc W_ho].T    shape:(4H, H)
+        self.B_h = np.random.rand(1, 4*self.H)          # B_h = [B_xf B_xi B_xc B_xo]       shape:(1, 4H)   B-(1, H)
+        self.W_hy = np.random.rand(self.K, self.H)      # W_hy  shape:(K, H)
+        self.B_y = np.random.rand(1, self.K)            # B_y   shape:(1, K)
 
         self._memory = False    # indicates whether subsequent batch sequence has relation with the last one
 
-        # the hidden_state and cell_state for time slot 0.
+        # initialize the hidden_state and cell_state for time slot 0.
         self.hidden_state = np.zeros((1, self.H))
         self.cell_state = np.zeros((1, self.H))
 
@@ -138,13 +150,24 @@ class LSTM(base_layer):
         self.layer = []
 
     def forward(self, input_tensor):
+        """
+        This function implement forward propagation of LSTM layer with help of the forward
+        propagation function of LSTM-cell
+        Args:
+            input_tensor: input tensor of LSTM layers, its shape should be (batch_size, input_size)
+        Returns:
+            output_tensor: output tensor of LSTM layers, its shape is (batch_size, output_size)
+        """
         batch_size = input_tensor.shape[0]      # batch size: input_tensor.shape[0]
         output_tensor = np.zeros((batch_size, self.K))
 
+        # determine whether sequential batches have relation. If memory is true, the hidden_state
+        # and cell_state of last cell will be transferred to the next cell
         if not self._memory:  # if the next batch(time batch) has no relation with the previous batch
             self.hidden_state = np.zeros((1, self.H))   # initialize hidden_state as 0
             self.cell_state = np.zeros((1, self.H))     # initialize cell_state as 0
 
+        # build LSTM layer based on LSTM cells
         for t in range(batch_size):
             cell = LSTM_cell(self.W_xh, self.W_hh, self.B_h, self.W_hy, self.B_y)
             output_tensor[t, :], self.hidden_state, self.cell_state = \
@@ -154,13 +177,21 @@ class LSTM(base_layer):
         return output_tensor
 
     def backward(self, error_tensor):
-        # error_tensor (B,K)  output_error (B,J)
+        """
+        This function implement backward propagation of LSTM layer with help of the backward
+        propagation function of LSTM-cell
+        Args:
+            error_tensor: error of LSTM layers which is transferred from next layer, shape should be (B,K)
+        Returns:
+            output_error: output error transferred from LSTM layers to last layer, shape is (batch_size, output_size)
+        """
+
+        # initialization
         # initialize output_error
         batch_size = error_tensor.shape[0]
         output_error = np.zeros((batch_size, self.J))
         # initialize hidden_error tensor and cellstate_error for last layer
-        dnext_c = np.zeros((1, self.H))
-        dnext_h = np.zeros((1, self.H))
+        dnext_c, dnext_h = np.zeros((1, self.H)), np.zeros((1, self.H))
         # initialization of gradients
         # W_xh:(4H, J)  W_hh:(4H, H)  B_h:(1, 4H)  W_hy:(K, H)  B_y:(1, K)
         grad_W_xh, grad_W_hh, grad_W_hy, grad_B_h, grad_B_y = \
@@ -171,6 +202,7 @@ class LSTM(base_layer):
             output_error[t, :], dnext_c, dnext_h, dW_xh, dW_hh, dB_h, dW_hy, dB_y =\
                 self.layer[t].backward(error_tensor[t, :], dnext_c, dnext_h)
 
+            # accumulation of gradients of LSTM-cells
             grad_W_xh += dW_xh
             grad_W_hh += dW_hh
             grad_B_h += dB_h
@@ -188,10 +220,11 @@ class LSTM(base_layer):
             self.B_h = self.B_h_optimizer.calculate_update(self.B_h, grad_B_h)
             self.B_y = self.B_y_optimizer.calculate_update(self.B_y, grad_B_y)
 
-        # 在一次反向传播更新后，重置RNN的状态
+        # After one propagation process, the memory flag will be re-initialized.
         self._memory = False
         return output_error
 
+    # initialization of weights and biases
     def initialize(self, weights_initializer, bias_initializer):    #fucntion which can reinitialize weights and bias
         # for W_xh, B_h: fan_in = input_size   fan_out=hidden_size
         self.W_xh = weights_initializer.initialize(self.W_xh.shape, self.J, self.H)
@@ -210,6 +243,7 @@ class LSTM(base_layer):
     def memorize(self, mem):
         self._memory = mem
 
+    # property of weights, which is used to make LSTM compatible with other layers
     @property
     def weights(self):
         # weights only include W_xh, W_hh, B_h. It means the parameters needed for calculation of hidden state.
@@ -232,10 +266,12 @@ class LSTM(base_layer):
             self.W_hh = None
             self.B_h = None
 
+    # property to get gradients
     @property
     def gradient_weights(self):
         return self.grad_weights
 
+    # property to get and set optimizers
     @property
     def optimizer(self):
         return self._optimizer
