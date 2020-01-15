3f74fc4fb291839a69e72472f2d662aead5490c2
Summabonas <51034065+Summabonas@users.noreply.github.com>
2020-01-15 10:05:07 +0100

    New comments of Base.py

--- Layers/Base.py
+++ Layers/Base.py
@@ -4,11 +4,11 @@ class base_layer:
     def __init__(self):
         self.phase = 'train'
         # self.weights is defined for not-trainable layers
-        # 1. we can know whether the layer is trainable by checking whether weights is equal to zero.
+        # 1. we can know whether the layer is trainable by checking whether weights is None.
         # 2. if it's trainable, we can employ "layer.optimizer.regularizer.norm(weights)" to calculate norm loss
         self.weights = None
 
-class Phase:            #属性引用 因为NeuralNetworkTests使用了属性引用，所以需要添加
+class Phase:
     test = 'test'
     train = 'train'
 
